959c7ddfa24fc580b33c86a8590d7356b3c660db
AndiraFaqih <afaqihm@gmail.com>
2023-09-27 02:02:17 +0700

    feat: adding login and logout for user

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,8 @@
 //import db, auth and storage from config/firebaseConfig.js
-const { createUserWithEmailAndPassword } = require('firebase/auth');
+const { createUserWithEmailAndPassword, 
+        signInWithRedirect, 
+        signInWithEmailAndPassword 
+    } = require('firebase/auth');
 const { auth } = require('../config/firebaseConfig');
 const bcrypt = require('bcrypt');
 const db = require('../models/index');
@@ -37,3 +40,79 @@ exports.signUpUser = async (req, res) => {
         res.status(400).send({ errorCode, errorMessage });
     }
 };
+
+//sign up and create account with google signinwithredirect
+exports.signUpWithGoogle = async (req, res) => {
+    let token;
+    try {
+        const userCredential = await signInWithRedirect(
+            auth,
+            new GoogleAuthProvider()
+        );
+        const uid = userCredential.user.uid;
+        token = await userCredential.user.getIdToken();
+        const userSchema = {
+            email: userCredential.user.email,
+            password: userCredential.user.password,
+            createdAt: new Date().toISOString(),
+        };
+        await db.collection('Users').doc(uid).set(userSchema);
+        res.status(201).send({
+            message: "User berhasil ditambahkan",
+            token: token
+        });
+    } catch (error) {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        res.status(400).send({ errorCode, errorMessage });
+    }
+};
+
+//login user with comparing password with bcrypt
+exports.loginUser = async (req, res) => {
+    let token;
+    const user = {
+        email: req.body.email,
+        password: req.body.password,
+    };
+
+    const userCredential = await db.collection('Users').where('email', '==', user.email).get();
+
+    if (userCredential.empty) {
+        return res.status(400).send({ message: "Email tidak ditemukan" });
+    }
+
+    const userSchema = userCredential.docs[0].data();
+    const isPasswordMatch = await bcrypt.compare(user.password, userSchema.password);
+
+    if (!isPasswordMatch) {
+        return res.status(400).send({ message: "Password salah" });
+    }
+    
+    try {
+        const userCredential = await signInWithEmailAndPassword(
+            auth,
+            user.email,
+            userSchema.password
+        );
+        token = await userCredential.user.getIdToken();
+        res.status(200).send({ message: "Login berhasil", token: token });
+    } catch (error) {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        res.status(400).send({ errorCode, errorMessage });
+    }
+};
+
+//sign out user
+exports.logoutUser = async (req, res) => {
+    try {
+        await auth.signOut();
+        res.status(200).send({ message: "Sign out berhasil" });
+    } catch (error) {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        res.status(400).send({ errorCode, errorMessage });
+    }
+}
+
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,5 +4,7 @@ const  controller  = require("../controllers/authController");
 const router = express.Router();
 
 router.post("/signup", controller.signUpUser);
+router.post("/login", controller.loginUser);
+router.post("/logout", controller.logoutUser);
 
 module.exports = router;
