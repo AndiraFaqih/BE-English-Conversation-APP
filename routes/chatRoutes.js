c11e70522b55562ec9409485cd7b529f1873129d
AndiraFaqih <afaqihm@gmail.com>
2023-10-03 23:21:01 +0700

    feat: adding middleware to the route

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const  controller  = require("../controllers/chatController");
+const { authCheck, isUser } = require("../middlewares/auth");
 const router = express.Router();
 
-router.post("/chat", controller.postChat);
+router.post("/chat",authCheck, isUser, controller.postChat);
 
 module.exports = router;
