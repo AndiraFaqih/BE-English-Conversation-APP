7b70f1638e4db94176b0b54b0122c874e77def2d
Rayhan Illyas <rayhan205732@gmail.com>
2024-02-11 01:07:21 +0700

    refactor: change route name for postChat

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -3,10 +3,10 @@ const  controller  = require("../controllers/chatController");
 const { authCheck, isUser } = require("../middlewares/auth");
 const router = express.Router();
 
-router.post("/roomchat/:chatRoomId/chat",authCheck, isUser, controller.postChat);
-router.post("/roomchat/:chatRoomId/speech",authCheck, isUser, controller.postChatText);
-router.put("/roomchat/:chatRoomId/chat/:idMessage", authCheck, isUser, controller.editChat);
-router.put("/roomchat/:chatRoomId/speech/:idMessage", authCheck, isUser, controller.editChatText);
+router.post("/roomchat/:chatRoomId/text-to-audio",authCheck, isUser, controller.postChat);
+router.post("/roomchat/:chatRoomId/text-to-text",authCheck, isUser, controller.postChatText);
+router.put("/roomchat/:chatRoomId/text-to-audio/:idMessage", authCheck, isUser, controller.editChat);
+router.put("/roomchat/:chatRoomId/text-to-text/:idMessage", authCheck, isUser, controller.editChatText);
 router.delete("/roomchat/:chatRoomId/chat/:idMessage", authCheck, isUser, controller.deleteChat);
 
 router.post("/roomchat",authCheck, isUser, controller.createChatRoom);
